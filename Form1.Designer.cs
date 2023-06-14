e26607d5b04797199c94dfd28cb37aacc22fc407
Daniele Alberti <kadmos@kadmos.com>
2023-06-14 11:10:39 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace DXFThumbNail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DXFReaderNET.DxfDocument dxfDocument1 = new DXFReaderNET.DxfDocument();
+            DXFReaderNET.AciColor aciColor1 = new DXFReaderNET.AciColor();
+            DXFReaderNET.Objects.RasterVariables rasterVariables1 = new DXFReaderNET.Objects.RasterVariables();
+            this.dxfReaderNETControl1 = new DXFReaderNET.DXFReaderNETControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dxfReaderNETControl1
+            // 
+            this.dxfReaderNETControl1.AutoSize = true;
+            this.dxfReaderNETControl1.ContinuousHighlight = false;
+            this.dxfReaderNETControl1.CustomCursor = DXFReaderNET.CustomCursorType.None;
+            dxfDocument1.ActiveLayout = "Model";
+            dxfDocument1.Comments = null;
+            aciColor1.Index = ((short)(256));
+            aciColor1.IsByBlock = false;
+            aciColor1.IsByLayer = true;
+            aciColor1.UseTrueColor = false;
+            dxfDocument1.CurrentColor = aciColor1;
+            dxfDocument1.CurrentElevation = 0D;
+            dxfDocument1.CurrentLayer = "0";
+            dxfDocument1.CurrentLineTypeName = "ByLayer";
+            dxfDocument1.CurrentLineTypeScale = 1D;
+            dxfDocument1.CurrentTextSize = 2.5D;
+            dxfDocument1.CurrentTextStyle = "STANDARD";
+            dxfDocument1.CurrentThickness = 0D;
+            dxfDocument1.DateCreated = new System.DateTime(2023, 6, 14, 9, 53, 51, 332);
+            dxfDocument1.DateLastEdited = new System.DateTime(2023, 6, 14, 9, 53, 51, 332);
+            dxfDocument1.Handle = "0";
+            dxfDocument1.IsBinary = false;
+            dxfDocument1.Modified = false;
+            dxfDocument1.Name = null;
+            dxfDocument1.Owner = null;
+            rasterVariables1.DisplayFrame = true;
+            rasterVariables1.DisplayQuality = DXFReaderNET.Objects.ImageDisplayQuality.High;
+            rasterVariables1.Handle = "2A";
+            rasterVariables1.Owner = null;
+            rasterVariables1.Units = DXFReaderNET.ImageUnits.Unitless;
+            dxfDocument1.RasterVariables = rasterVariables1;
+            dxfDocument1.ThumbnailImage = null;
+            dxfDocument1.TotalEditTime = new System.DateTime(2023, 6, 14, 0, 0, 0, 0);
+            dxfDocument1.TotalEditTimeDays = 0;
+            dxfDocument1.UserTimer = new System.DateTime(2023, 6, 14, 0, 0, 0, 0);
+            dxfDocument1.UserTimerDays = 0;
+            this.dxfReaderNETControl1.DXF = dxfDocument1;
+            this.dxfReaderNETControl1.FileName = null;
+            this.dxfReaderNETControl1.HighlightGrabPoints = false;
+            this.dxfReaderNETControl1.HighlightGrabPointsColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(163)))), ((int)(((byte)(0)))));
+            this.dxfReaderNETControl1.HighlightMarkerColor2 = System.Drawing.Color.Magenta;
+            this.dxfReaderNETControl1.HighlightNotContinuous = false;
+            this.dxfReaderNETControl1.Location = new System.Drawing.Point(12, 12);
+            this.dxfReaderNETControl1.Name = "dxfReaderNETControl1";
+            this.dxfReaderNETControl1.ShowAxes = false;
+            this.dxfReaderNETControl1.ShowGrid = true;
+            this.dxfReaderNETControl1.ShowGridRuler = false;
+            this.dxfReaderNETControl1.Size = new System.Drawing.Size(500, 500);
+            this.dxfReaderNETControl1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(530, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open DXF file...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(530, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Show thumbnail";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(530, 119);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 220);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(533, 351);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 159);
+            this.label1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 519);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dxfReaderNETControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "DXFReader ThumbNail Test";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DXFReaderNET.DXFReaderNETControl dxfReaderNETControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using DXFReaderNET.Entities;
+using DXFReaderNET;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DXFThumbNail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.DefaultExt = "DXF";
+            openFileDialog1.Filter = "DXF|*.dxf";
+            openFileDialog1.FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                dxfReaderNETControl1.ReadDXF(openFileDialog1.FileName);
+                dxfReaderNETControl1.HighlightNotContinuous = true;
+                dxfReaderNETControl1.ZoomCenter();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DXFReaderNETControl _dxfControl;
+
+            _dxfControl = new DXFReaderNETControl();
+
+            _dxfControl.ReadDXF(dxfReaderNETControl1.FileName);
+
+            _dxfControl.Width = 1000;
+            _dxfControl.Height = 1000;
+
+            _dxfControl.AntiAlias = false;
+            _dxfControl.ShowAxes = false;
+            _dxfControl.ShowGrid = false;
+            _dxfControl.BackColor = System.Drawing.Color.White;
+            _dxfControl.ForeColor = System.Drawing.Color.Black;
+
+
+            ShowFilledAreas(_dxfControl);
+
+            _dxfControl.ZoomCenter();
+            pictureBox1.Image = _dxfControl.Image;
+          
+            _dxfControl.Dispose();
+
+            int InternalCountoursNumber = 0;
+            double ExternalLenght = 0;
+            double ExternalArea = 0;
+            double InternalLenght = 0;
+            double InternalArea = 0;
+            bool ret = MathHelper.FindClosedAreaData(dxfReaderNETControl1.DXF.Entities, out ExternalLenght, out ExternalArea, out InternalLenght, out InternalArea, out InternalCountoursNumber);
+
+            if (ret)
+            {
+
+                label1.Text = "External lenght: " + dxfReaderNETControl1.DXF.ToFormattedUnit(ExternalLenght) + "\r\n";
+                label1.Text += "External area: " + dxfReaderNETControl1.DXF.ToFormattedUnit(ExternalArea) + "\r\n";
+                label1.Text += "Internal lenght: " + dxfReaderNETControl1.DXF.ToFormattedUnit(InternalLenght) + "\r\n";
+                label1.Text += "Internal area: " + dxfReaderNETControl1.DXF.ToFormattedUnit(InternalArea) + "\r\n";
+                label1.Text += "Filled area: " + dxfReaderNETControl1.DXF.ToFormattedUnit(ExternalArea - InternalArea) + "\r\n";
+                label1.Text += "Internal countours #: " + InternalCountoursNumber.ToString() + "\r\n";
+
+                Vector2 extension = new Vector2(dxfReaderNETControl1.DXF.DrawingVariables.ExtMax.X - dxfReaderNETControl1.DXF.DrawingVariables.ExtMin.X, dxfReaderNETControl1.DXF.DrawingVariables.ExtMax.Y - dxfReaderNETControl1.DXF.DrawingVariables.ExtMin.Y);
+                label1.Text += "Extension : " + dxfReaderNETControl1.DXF.ToFormattedUnit(extension.X) + " x " + dxfReaderNETControl1.DXF.ToFormattedUnit(extension.Y);
+
+            }
+        }
+
+        private void ShowFilledAreas(DXFReaderNETControl myDXF)
+        {
+
+
+            List<EntityObject> externalContour = MathHelper.ExternalContour(myDXF.DXF.Entities);
+
+            List<EntityObject> allEntities = new List<EntityObject>();
+            foreach (EntityObject entity in myDXF.DXF.Entities)
+            {
+                if (!externalContour.Contains(entity))
+                    allEntities.Add(entity);
+            }
+
+            List<LwPolyline> lws = dxfReaderNETControl1.EntitiesToLwPolylines(allEntities);
+
+            List<EntityObject> BoundaryOutermost = new List<EntityObject>();
+            foreach (LwPolyline l in lws)
+            {
+
+                BoundaryOutermost.Add(l);
+            }
+
+
+            EntityObject myEnt = myDXF.AddGradientHatch(HatchGradientPatternType.Cylinder, externalContour, BoundaryOutermost, 140, 180, 30, true, "SERVICE");
+
+         
+
+        }
+
+    }
+}
